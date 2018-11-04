9782c24f17923e35f26faad97011643f963ac7b2
Deepak <d.kadarivel@relay42.com>
2018-11-04 11:10:15 +0100

    react router config update

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
 
 import LoginComponent from './components/login/LoginComponent';
 import App from './components/app/App';
 
 
 export default function Routes() {
-  const history = createHistory({
-    basename: process.env.PUBLIC_URL
-  });
-
   return (
-    <BrowserRouter history={history}>
-      <Switch>
-        <Route component={() => <div>404 Not found 1</div>}/>
-        <Route exact path="/" component={App}/>
+    <BrowserRouter>
+      <Switch basename={window.location.pathname || ''}>
         <Route path="/login" component={LoginComponent}/>
+        <Route component={App}/>
       </Switch>
     </BrowserRouter>
   );
