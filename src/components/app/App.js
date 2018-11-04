04e0dc331aec2500c9f47f29f4f59494d91efd48
Deepak <d.kadarivel@relay42.com>
2018-11-04 10:20:22 +0100

    Merge branch 'master' of github.com:deepakkadarivel/ippwa
    # Conflicts:
    #	package.json
    
    Fixed router configuration.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import './App.scss';
 import LoginComponent from '../login/LoginComponent';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <CssBaseline/>
-          <Route exact path={process.env.PUBLIC_URL + '/login'} component={LoginComponent}/>
+      <div className="App">
+        <CssBaseline/>
+        <Switch basename={window.location.pathname || ''}>
+          <Route exact path='/login' component={LoginComponent}/>
           {/*<Route exact path="/about" component={About} />*/}
           {/*<Route exact path="/code" component={Code} />*/}
           {/*<Route exact path="/contact" component={Contact} />*/}
           {/*<Route exact path="/presence" component={info} />*/}
-        </div>
-      </Router>
+        </Switch>
+      </div>
     );
   }
 }
